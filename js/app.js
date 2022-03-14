f23c81be15621e4ffc83650629491573bc19d549
mdsaharshital <mdsaharshitol@gmail.com>
2022-03-14 17:27:57 +0600

    product price updown  done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-console.log('here');
+const plusMinus =( isIncreasing)=>{
+    let productInput = document.getElementById('product-number');
+    let productNumber = productInput.value;
+    if(isIncreasing){
+        productNumber = getInputValue() + 1;
+    }
+    else if( productNumber > 0){
+        productNumber = getInputValue() - 1;
+    }
+    productInput.value = productNumber;
+    updatePrice(isIncreasing, productNumber);
+}
+function getInputValue() {
+    const productInput = document.getElementById('product-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+const updatePrice =(isIncreasing)=>{
+    const productPriceText = document.getElementById('product-price');
+    let productPrice = productPriceText.innerText;
+    productPrice = getInputValue() *125 ;
+    productPriceText.innerText = productPrice;
+}
+const plusClick =()=>{
+    plusMinus(true)
+}
+const minusClick =()=>{
+    plusMinus(false)
+}
